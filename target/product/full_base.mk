112b64a0bc5a589521c442c23dc9efb03fe0f4a0
Christian Gaetano <cgatno@gmail.com>
2013-11-25 01:31:56 -0500

    We don't need to get these packages here anymore

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -38,12 +38,6 @@ PRODUCT_LOCALES := en_US
 # Include drawables for all densities
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
-# Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
-
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
